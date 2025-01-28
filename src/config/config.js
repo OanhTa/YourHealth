805203be9f14abc79dd49552597ddd10fb5d7787
Tạ Kim Oanh <oanhta20032003@gmail.com>
2025-01-28 10:26:57 +0700

    change db

--- src/config/config.js
+++ src/config/config.js
@@ -1,31 +1,38 @@
-const config = {
-  development: {
-    username: "root",
-    password: null,
-    database: "quanlyuser",
-    host: "127.0.0.1",
-    dialect: "mysql",
-    logging: false,
-    query: {
-      raw: true
+require('dotenv').config(); // this is important!
+module.exports = {
+  "development": {
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE_NAME,
+    "host": process.env.DB_HOST,
+    "port": process.env.DB_PORT,
+    "dialect": process.env.DB_DIALECT,
+    "define": {
+      "freezeTableName": true
     },
-    timezone: '+07:00',
-    logging: console.log, // Log all SQL queries
+    dialectOptions:
+      process.env.DB_SSL === 'true' ?
+        {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false
+          }
+        } : {}
+    ,
+    "timezone": "+07:00"
   },
-  test: {
-    username: "root",
-    password: null,
-    database: "database_test",
-    host: "127.0.0.1",
-    dialect: "mysql"
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
   },
-  production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
-    dialect: "mysql"
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
   }
 };
-
-module.exports = config;
--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,20 +1,38 @@
 const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
 
-// Option 3: Passing parameters separately (other dialects)
-const sequelize = new Sequelize('quanlyuser', 'root' , null , {
-  host: 'localhost',
-  dialect: 'mysql',/* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
-  logging: false
-});
+// Option 2: Passing parameters separately (other dialects)
+const sequelize = new Sequelize(
+    process.env.DB_DATABASE_NAME,
+    process.env.DB_USERNAME,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: 'postgres',
+        logging: false,
+        dialectOptions:
+            process.env.DB_SSL === 'true' ?
+                {
+                    ssl: {
+                        require: true,
+                        rejectUnauthorized: false
+                    }
+                } : {}
+        ,
+        query: {
+            "raw": true
+        },
+        timezone: "+07:00"
+    });
 
-let connectDB = async ()=>{
+let connectDB = async () => {
     try {
         await sequelize.authenticate();
         console.log('Connection has been established successfully.');
-      } catch (error) {
+    } catch (error) {
         console.error('Unable to connect to the database:', error);
-      }
+    }
 }
-
 module.exports = connectDB;
